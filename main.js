44545f87b729ee5b46d7169bc092d5560abbcbfd
Pavithra-U <86193022+Pavithra-U@users.noreply.github.com>
2022-02-10 18:42:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/f2gzMvzIr/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+        
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+
+    gesture = results[0].label;
+    
+    toSpeak = "";
+    
+    if(gesture == "Thumbs Down")
+    {
+      toSpeak = "Thumbs down";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#128076;";
+    }
+    else if(gesture == "Thumbs Up")
+    {
+      toSpeak = "Thumbs Up";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;";
+    }
+    else if(gesture == "Victory")
+    {
+      toSpeak = "That was the marvelous victory";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;";
+    }
+    else if(gesture == "Clap")
+    {
+      toSpeak = "That was great";
+      document.getElementById("result_object_gesture_icon").innerHTML = "&#128079;";
+    }
+    speak();
+  }
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+
+    speak_data = toSpeak;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+}
